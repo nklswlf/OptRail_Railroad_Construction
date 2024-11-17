a8a3c0f400d45b80e5834724c25f603c2b4d86d8
nklswlf <wlfnkls@gmail.com>
2024-11-17 12:35:41 +0100

    :construction: Build MIP Step by Step

--- Code/MIP_Flow_Formulation.py
+++ Code/MIP_Flow_Formulation.py
@@ -65,8 +65,8 @@ def Run_MIP():
     start_date = data.start_date
     end_date = data.end_date
 
-
-    O_t = dict()  # Startzeiten --> im Modell angegeben    
+    O_t = dict()  # Tag an dem der Auftrag startet
+    O_t_start = dict()  # Startzeiten  
     O_t_end = dict()  # Endzeiten
     O_t_start_inverted = dict()  # Umgekehrtes O_t (Startzeiten)
     O_t_end_inverted = dict()  # Umgekehrtes O_t_end (Endzeiten)
@@ -80,11 +80,19 @@ def Run_MIP():
         orderItem_start_date = orderItem.start_time
         delta_start = (orderItem_start_date - start_date)
         t_start = delta_start.total_seconds() / SECONDS_IN_A_DAY
+        t_start_int = int(t_start)
+
+
+        # O_t: Gruppiert nach Tagen
+        if t_start_int not in O_t:
+            O_t[t_start_int] = []
+        O_t[t_start_int].append(orderID)
         
-        # Original O_t: Gruppiert nach Startzeit
-        if t_start not in O_t:
-            O_t[t_start] = []
-        O_t[t_start].append(orderID)
+        
+        # Startzeit
+        if t_start not in O_t_start:
+            O_t_start[t_start] = []
+        O_t_start[t_start].append(orderID)
         
         # Invertiertes Dictionary O_t_start_inverted
         O_t_start_inverted[orderID] = t_start
@@ -107,6 +115,10 @@ def Run_MIP():
     P_mn = dict()
     S_mn = dict()
 
+    d_ij = data.transport_routes
+    d_wj = data.work_routes
+    SPEED = 1680 # Durchschnittliche Geschwindigkeit des Maschinentransports in 1680 km/Tag --> entspricht 70 km/h
+
     for m in M:
         for n in N_m[m]:
             P_mn[m,n] = list()
@@ -118,17 +130,17 @@ def Run_MIP():
                     start_time_i = O_t_start_inverted[i]
                     end_time_i = O_t_end_inverted[i]
 
-                    if start_time_n > end_time_i:  # Mit Travel time und kleiner gleich
+                    if start_time_n >= end_time_i + d_ij[i][n] / SPEED: 
                         P_mn[m,n].append(i)
 
-                    if start_time_i > end_time_n:
+                    if start_time_i > end_time_n + d_ij[n][i] / SPEED:
                         S_mn[m,n].append(i)
 
 
     P_wn = dict()
     S_wn = dict()
 
-    # Hier muss die Ruhezeit noch berücksichtigt werden
+    P_time = 9/24 # 9 Stunden Pausenzeit zwischen zwei Schichten --> 9/24 Tage
 
     for w in W:
         for n in N_w[w]:
@@ -141,10 +153,10 @@ def Run_MIP():
                     start_time_i = O_t_start_inverted[i]
                     end_time_i = O_t_end_inverted[i]
 
-                    if start_time_n > end_time_i:
+                    if start_time_n >= end_time_i + P_time:
                         P_mn[m,n].append(i)
 
-                    if start_time_i > end_time_n:
+                    if start_time_i >= end_time_n + P_time:
                         S_mn[m,n].append(i)
             
 
@@ -180,14 +192,14 @@ def Run_MIP():
 
 
     # 3. Variablen erstellen
-    x = model.addVars(M, N, N, vtype=GRB.BINARY, name="x")  # Maschinenflussvariablen
-    y = model.addVars(W, N, N, vtype=GRB.BINARY, name="y")  # Arbeiterflussvariablen
+    x = model.addVars(((m, i, j) for m in M for i in N_m[m] for j in N_m[m]),vtype=GRB.BINARY,name="x")  # Maschinenflussvariablen
+    y = model.addVars(((w, i, j) for w in W for i in N_w[w] for j in N_w[w]),vtype=GRB.BINARY,name="y")  # Arbeiterflussvariablen
     
     '''
     z = model.addVars(A, N, N, vtype=GRB.BINARY, name="z")  # Anbaugeräteflussvariablen --> ANNAHME
     '''
 
-    s = model.addVars(M, N, vtype=GRB.BINARY, name="s")     # Non-regular driver Nutzung
+    s = model.addVars(((m, i) for m in M for i in N_m[m]),vtype=GRB.BINARY,name="s")    # Non-regular driver Nutzung
     u = model.addVars(C, vtype=GRB.BINARY, name="u")     # (Komplette) Baustellen-Erfüllung True/False
 
 
@@ -196,7 +208,7 @@ def Run_MIP():
     model.setObjective(
         gp.quicksum(100 * u[k] for k in C) -  # Baustellen-Erfüllung --> Fällt bspw. 100x ins Gewicht
         gp.quicksum(d_ij[i, j] * x[m, i, j] for m in M for i in N_m[m] for j in N_m[m]) + # Transportaufwand Maschinen
-        gp.quicksum(d_ij[i, j] * y[w, i, j] for w in W for i in N for j in N) + # Arbeitswegeaufwand Arbeiter        
+        gp.quicksum(d_wj[w, j] * y[w, i, j] for w in W for i in N for j in N) + # Arbeitswegeaufwand Arbeiter        
         gp.quicksum(s[m, i] for m in M for i in N), # Strafkosten für Non-regular driver Nutzung
         GRB.MAXIMIZE
     )
@@ -209,19 +221,39 @@ def Run_MIP():
     # 5. Nebenbedingungen
     # Maschinenfluss-Balance
     for m in M:
-        for i in N:
+        for i in N_m[m]:
             model.addConstr(
-                gp.quicksum(x[m, j, i] for j in N) == gp.quicksum(x[m, i, j] for j in N),
+                gp.quicksum(x[m, j, i] for j in P_mn[m,i]) == gp.quicksum(x[m, i, j] for j in S_mn[m, i]),
                 name=f"machine_flow_balance_{m}_{i}"
             )
 
+
+    for m in M:
+        for s in N_m[m]:
+            for t in N_m[m]:
+                if s != t:
+                    left_sum = gp.quicksum(x[m, s, j] for j in S_mn[m,s] if j != t)
+                    right_sum = gp.quicksum(x[m, i, t] for i in P_mn[m,t] if i != s)
+                    model.addConstr(left_sum == right_sum, name=f"machine_balance_{m}_{s}_{t}")
+
+
     # Arbeiterfluss-Balance
     for w in W:
-        for i in N:
+        for i in N_w[w]:
             model.addConstr(
-                gp.quicksum(y[w, j, i] for j in N) == gp.quicksum(y[w, i, j] for j in N),
+                gp.quicksum(y[w, j, i] for j in P_wn[w,i]) == gp.quicksum(y[w, i, j] for j in S_wn[w,i]),
                 name=f"worker_flow_balance_{w}_{i}"
             )
+
+
+    for w in W:
+        for s in N_w[w]:
+            for t in N_w[w]: 
+                if s != t: 
+                    left_sum = gp.quicksum(y[w, s, j] for j in S_wn[w, s] if j != t)
+                    right_sum = gp.quicksum(y[w, i, t] for i in P_wn[w, t] if i != s)
+                    model.addConstr(left_sum == right_sum, name=f"worker_balance_w_{w}_s{s}_t{t}")
+
 
     '''
     # Anbaugerätefluss-Balance
@@ -235,13 +267,33 @@ def Run_MIP():
             
     # Regelmäßige Fahrer - Nebenbedingung
     for m in M:
-        for i in N:
+        for i in N_m[m]:
             model.addConstr(
-                gp.quicksum(x[m, i, j] for j in N) <= gp.quicksum(y[w, i, j] for w in W for j in N) + s[m, i],
+                gp.quicksum(x[m, i, j] for j in S_mn[m,i]) <= gp.quicksum(y[w, i, j] for w in W_m[m] for j in S_wn[w,i]) + s[m, i],
                 name=f"regular_driver_constraint_{m}_{i}"
             )
 
-    # Weitere Constraints können gemäß dem Modell hinzugefügt werden...
+    # Baustellen-Erfüllung
+    for k in C:
+        for i in N_c[k]:
+            model.addConstr(
+                gp.quicksum(x[m, i, j] for m in M for j in S_mn[m,i]) == u[k],
+                name=f"machine_site_fulfillment_{k}_{i}"
+            )
+            model.addConstr(
+                gp.quicksum(y[w, i, j] for w in W for j in S_wn[w,i]) == u[k],
+                name=f"worker_site_fulfillment_{k}_{i}"
+            )
+
+    # Nachschicht-Beschränkung
+    for w in W:
+        for i in N_w[w]:
+            for t in T_range:
+                model.addConstr(
+                    gp.quicksum(y[w, i, j] for j in S_wn[w,i] if j in T_range) <= S_Nmax,
+                    name=f"night_shift_constraint_{w}_{i}_{t}"
+                )
+
 
     # 6. Optimierung
     model.optimize()
