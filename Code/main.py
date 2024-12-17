7b28c83946e69bf0ef132a39935d960e68416292
nklswlf <wlfnkls@gmail.com>
2024-12-17 18:01:43 +0100

    :zap: Quick main change

--- Code/main.py
+++ Code/main.py
@@ -7,83 +7,69 @@ from pathlib import Path
 
 # Reduced
 '''
-Construction_a1_o12_m3_an5_ar3_reduced
-Construction_a3_o80_m10_an10_ar9_reduced
-Construction_a5_o96_m10_an10_ar10_reduced
+Construction_a1_o12_m3_an5_ar3_reduced.json
+Construction_a3_o80_m10_an10_ar9_reduced.json
+Construction_a5_o96_m10_an10_ar10_reduced.json
 '''
 
 # 10 Sites --> "Construction_a10_o118_m6_an53_ar13.json": Instance not duable since one order has no order items
 '''
-Construction_a10_o107_m5_an57_ar12
-Construction_a10_o128_m6_an51_ar13
-Construction_a10_o144_m6_an53_ar12
+Construction_a10_o107_m5_an57_ar12.json
+Construction_a10_o114_m6_an57_ar11.json
+Construction_a10_o119_m5_an54_ar13.json
+Construction_a10_o128_m6_an51_ar13.json
+Construction_a10_o144_m6_an53_ar12.json
 '''
 
 # 15 Sites
 '''
-Construction_a15_o170_m9_an80_ar18
-Construction_a15_o195_m8_an81_ar20
-Construction_a15_o225_m9_an75_ar19
+Construction_a15_o170_m9_an80_ar18.json
+Construction_a15_o191_m8_an74_ar18.json
+Construction_a15_o195_m8_an81_ar20.json
+
 '''
 
 # 20 Sites
 '''
-Construction_a20_o236_m12_an106_ar24
-Construction_a20_o268_m11_an118_ar26
-Construction_a20_o299_m12_an98_ar29
+Construction_a20_o236_m12_an106_ar24.json
+Construction_a20_o259_m11_an101_ar26.json
+Construction_a20_o268_m11_an118_ar26.json
 '''
 
 # 25 Sites
 '''
-Construction_a25_o306_m13_an127_ar31
-Construction_a25_o335_m16_an145_ar31
-Construction_a25_o360_m15_an149_ar34
+Construction_a25_o306_m13_an127_ar31.json
+Construction_a25_o335_m16_an145_ar31.json
+Construction_a25_o360_m15_an149_ar34.json
 '''
 
 # 30 Sites
 '''
-Construction_a30_o355_m18_an148_ar42
-Construction_a30_o397_m17_an150_ar42
-Construction_a30_o428_m16_an168_ar43
+Construction_a30_o355_m18_an148_ar42.json
+Construction_a30_o397_m17_an150_ar42.json
+Construction_a30_o428_m16_an168_ar43.json
 '''
 
 # 40 Sites
 '''
-Construction_a40_o476_m22_an215_ar51
-Construction_a40_o502_m21_an197_ar55
-Construction_a40_o551_m25_an221_ar53
+Construction_a40_o476_m22_an215_ar51.json
+Construction_a40_o502_m21_an197_ar55.json
+Construction_a40_o551_m25_an221_ar53.json
 '''
 
 # 50 Sites
 '''
-Construction_a50_o578_m28_an276_ar66
-Construction_a50_o639_m28_an269_ar63
-Construction_a50_o668_m29_an248_ar68
+Construction_a50_o578_m28_an276_ar66.json
+Construction_a50_o639_m28_an269_ar63.json
+Construction_a50_o668_m29_an248_ar68.json
 '''
 
 
-instances = [   "Construction_a10_o107_m5_an57_ar12.json",
-                "Construction_a10_o128_m6_an51_ar13.json",
-                "Construction_a10_o144_m6_an53_ar12.json",
-                "Construction_a15_o170_m9_an80_ar18.json",
-                "Construction_a15_o195_m8_an81_ar20.json",
-                "Construction_a15_o225_m9_an75_ar19.json",
-                "Construction_a20_o236_m12_an106_ar24.json",
-                "Construction_a20_o268_m11_an118_ar26.json",
-                "Construction_a20_o299_m12_an98_ar29.json",
-                "Construction_a25_o306_m13_an127_ar31.json",
-                "Construction_a25_o335_m16_an145_ar31.json",
-                "Construction_a25_o360_m15_an149_ar34.json",
-                "Construction_a30_o355_m18_an148_ar42.json",
-                "Construction_a30_o397_m17_an150_ar42.json",
-                "Construction_a30_o428_m16_an168_ar43.json",
-                "Construction_a40_o476_m22_an215_ar51.json",
-                "Construction_a40_o502_m21_an197_ar55.json",
-                "Construction_a40_o551_m25_an221_ar53.json",
-                "Construction_a50_o578_m28_an276_ar66.json",
-                "Construction_a50_o639_m28_an269_ar63.json",
-                "Construction_a50_o668_m29_an248_ar68.json"
+instances = ["Construction_a10_o119_m5_an54_ar13.json",
+             "Construction_a15_o191_m8_an74_ar18.json",
+             "Construction_a20_o236_m12_an106_ar24.json"
             ]
+
              
 
 
